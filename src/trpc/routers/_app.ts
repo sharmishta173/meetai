7944aace81b47172bea35ebef8517ac3961fa3f6
sharmishta173 <thamminenisharmishta21@gmail.com>
2026-02-09 19:47:05 +0530

    09: agents setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,9 @@
 import { z } from 'zod';
 import { baseProcedure, createTRPCRouter } from '../init';
+import { agentsRouter } from '@/modules/agents/server/procedures';
+
 export const appRouter = createTRPCRouter({
+  agents: agentsRouter,
   hello: baseProcedure
     .input(
       z.object({
