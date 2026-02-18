43a43d84bc2953747b86b69b29fa207c56655923
sharmishta173 <thamminenisharmishta21@gmail.com>
2026-02-18 23:48:42 +0530

    11: agents form

---
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -0,0 +1,128 @@
+ "use client";
+ 
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQueryClient } from "@tanstack/react-query"; 
+import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import{
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { AgentGetOne } from "../../type";
+import { agentsInsertSchema } from "../../schemas";
+
+interface AgentFormProps {
+    onSuccess?: () =>void;
+    onCancel?: () =>void;
+     initialValues?: AgentGetOne;
+};
+
+export const AgentForm  = ({
+    onSuccess,
+    onCancel,
+    initialValues,
+}: AgentFormProps) => {
+   const trpc = useTRPC();
+   const queryClient = useQueryClient();
+ 
+   const form = useForm<z.infer<typeof agentsInsertSchema>>({
+     resolver: zodResolver(agentsInsertSchema),
+     defaultValues: {
+       name: initialValues?.name ?? "",
+       instructions: initialValues?.instructions ?? "",
+     },
+   });
+ 
+   const createAgent = useMutation(
+     trpc.agents.create.mutationOptions({
+       onSuccess: async () => {
+         await queryClient.invalidateQueries({
+           queryKey: trpc.agents.getMany.queryKey(),
+         });
+         onSuccess?.();
+       },
+       onError: (error) => {
+         toast.error(error.message);
+
+         // TODO: Check if error code is "FORBIDDEN", redirect to "/upgrade"
+       },
+     }),
+   );
+ 
+  const isEdit = !!initialValues?.id;
+ 
+   const isPending = createAgent.isPending;
+ 
+   const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => { 
+     createAgent.mutate(values);
+   };
+ 
+   return (
+     <Form {...form}>
+       <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+         <GeneratedAvatar 
+           seed={form.watch("name")}
+           variant="botttsNeutral"
+           className="border size-16"   
+         />
+         <FormField   
+           name="name"
+           control={form.control}
+           render={({ field }) => (
+             <FormItem>
+               <FormLabel>Name</FormLabel>
+               <FormControl>
+                 <Input {...field} placeholder="e.g. Math tutor" />
+               </FormControl>
+               <FormMessage />
+             </FormItem>
+           )}
+         />
+         <FormField
+           name="instructions"
+           control={form.control}
+           render={({ field }) => (
+             <FormItem>
+               <FormLabel>Instructions</FormLabel>
+               <FormControl>
+                 <Textarea 
+                   rows={4}
+                   className="resize-none"
+                   {...field}
+                   placeholder="You are a helpful math assistant that can answer questions and help with assignments."
+                 />
+               </FormControl>
+               <FormMessage />
+             </FormItem>
+           )}
+         />
+         <div className="flex justify-between gap-x-2">
+            {onCancel && (
+                <Button
+                   variant="ghost"
+                   disabled={isPending}
+                   type="button"
+                   onClick={() => onCancel()}
+                   >
+                    Cancel
+                   </Button>
+            )}
+            <Button disabled={isPending} type="submit">
+                {isEdit ? "Update" : "Create"}
+
+            </Button>
+         </div>
+         </form>
+         </Form>
+   );
+ };
